aadbdce7feddec23b6361bfd7f2ee979abb564e7
Cleverton Santos <clevertonsantoscodev@gmail.com>
2026-02-17 15:51:08 -0300

    registrando interfaces

--- modules/Clinic/Providers/ClinicServiceProvider.php
+++ modules/Clinic/Providers/ClinicServiceProvider.php
@@ -3,15 +3,23 @@
 namespace Modules\Clinic\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Modules\Clinic\Contracts\TreatmentPlanRepositoryInterface;
+use Modules\Clinic\Contracts\TreatmentPlanServiceInterface;
 use Modules\Clinic\Models\Clinic;
 use Modules\Clinic\Observers\ClinicObserver;
+use Modules\Clinic\Repositories\TreatmentPlanRepository;
+use Modules\Clinic\Services\TreatmentPlanService;
 
 class ClinicServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->app->bind(TreatmentPlanRepositoryInterface::class, TreatmentPlanRepository::class);
+        $this->app->bind(TreatmentPlanServiceInterface::class, TreatmentPlanService::class);
+    }
 
     /**
      * Bootstrap any application services.

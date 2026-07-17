74a1a15d250f1f86a3d0fa9b2c31907d6391c6ff
Cleverton  Santos <clevertonsantoscodev@gmail.com>
2026-07-16 21:46:22 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-// SPA catch-all: exclui API e o Log Viewer (opcodesio/log-viewer)
 Route::get('/{any?}', fn () => view('app'))->where('any', '^(?!api(?:/|$)|log-viewer(?:/|$)).*$');
